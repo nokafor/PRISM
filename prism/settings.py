ca764f8603a2a4c005720a88092142de811838c1
nokafor <nokafor@princeton.edu>
2015-04-19 23:57:16 -0400

    updated heroku env

--- prism/settings.py
+++ prism/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../prism/static'),
 )
 
 #--- HEROKU, POSTGRES ---
