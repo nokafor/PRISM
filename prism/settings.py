0b6f4ea8ccdd956a62f332fdbfdb68f52459668e
nokafor <nokafor@princeton.edu>
2015-07-06 16:17:42 -0400

    updated heroku settings

--- prism/settings.py
+++ prism/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
---- HEROKU, POSTGRES ---
+# --- HEROKU, POSTGRES ---
 
 #Parse database configuration from $DATABASE_URL
 import dj_database_url
