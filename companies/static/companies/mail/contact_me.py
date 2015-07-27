59dd2fb86a4489000e7b49b87596f6b98c172fc3
nokafor <nokafor@princeton.edu>
2015-07-27 13:51:12 -0400

    contact form update

--- companies/static/companies/mail/contact_me.py
+++ companies/static/companies/mail/contact_me.py
@@ -6,7 +6,7 @@ message = sendgrid.Mail()
 message.add_to('nokafor@princeton.edu')
 message.set_subject('Example')
 message.set_text('Body')
-message.set_from('Doe John')
+message.set_from('sokafor15@gmail.com')
 status, msg = sg.send(message)
 
 return True
