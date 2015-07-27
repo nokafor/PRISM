cba269eba1e034f89ff1cafdf6eb0b597d0e08f9
nokafor <nokafor@princeton.edu>
2015-07-27 14:19:41 -0400

    contact form update

--- companies/static/companies/mail/contact_me.py
+++ companies/static/companies/mail/contact_me.py
@@ -1,10 +1,14 @@
 import sendgrid
 
-sg = sendgrid.SendGridClient('princetonism', 'PRISMfounder16')
+def index(request):
+	postData = request.form
+	email = str(postData['email'].value)
 
-message = sendgrid.Mail()
-message.add_to('nokafor@princeton.edu')
-message.set_subject('Example')
-message.set_text('Body')
-message.set_from('sokafor15@gmail.com')
-status, msg = sg.send(message)
+	sg = sendgrid.SendGridClient('princetonism', 'PRISMfounder16')
+
+	message = sendgrid.Mail()
+	message.add_to('nokafor@princeton.edu')
+	message.set_subject('Example')
+	message.set_text('Body')
+	message.set_from(email)
+	status, msg = sg.send(message)
